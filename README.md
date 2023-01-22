1c91ac4a1a7a136cbd5ac09b780a313a18c5b1e9
shanelevites <srlevit94@gmail.com>
2023-01-21 17:34:02 -0800

    added GET and POST requests to server.js

--- README.md
+++ README.md
@@ -31,15 +31,15 @@ The application should have a db.json file on the back end that will be used to 
 
 The following HTML routes should be created:
 
-[GET /notes] should return the notes.html file.
+-- [GET /notes] should return the notes.html file.
 
-[GET *] should return the index.html file.
+-- [GET *] should return the index.html file.
 
 The following API routes should be created:
 
-[GET /api/notes] should read the db.json file and return all saved notes as JSON.
+-- [GET /api/notes] should read the db.json file and return all saved notes as JSON.
 
-[POST /api/notes] should receive a new note to save on the request body, add it to the [db.json] file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into [npm] packages that could do this for you).
+-- [POST /api/notes] should receive a new note to save on the request body, add it to the [db.json] file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into [npm] packages that could do this for you).
 
 ## Bonus
 You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. As a bonus, see if you can add the DELETE route to the application using the following guideline:
--- server.js
+++ server.js
@@ -1,16 +1,58 @@
+// packages needed to run appliation
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { clog } = require('./middleware/clog');
 
 const app = express();
-const PORT = 3008;
+const PORT = process.env.port || 3001;
 
+// destination file for notes
+const { notes } = require('./db/db.json');
+
+//express middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(clog);
 
-app.get('/', (req, res) => res.send('Navigate to /notes'));
 
+// -- [GET /notes] should return the notes.html file.
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/notes.html')));
+    res.sendFile(path.join(__dirname, 'notes.html')));
 
-app.listen(PORT, () =>
-    console.log(`app listening on http://localhost:${PORT}`)
+// -- [GET *] should return the index.html file.
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html'))
 );
+
+// -- [GET /api/notes] should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => res.json(notes));
+
+// -- [POST /api/notes] should receive a new note to save on the request body, add it to the [db.json] file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into [npm] packages that could do this for you).
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { noteTitle, noteContent } = req.body;
+  
+    // If all the required properties are present
+    if (noteTitle  && noteContent) {
+      // Variable for the object we will save
+      const newNote = {
+        noteTitle,
+        noteContent,
+        note_id: uuid(),
+      };
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in creating Note');
+    }
+  });
