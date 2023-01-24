ca33625c38c91bad77f67969828f34e11b1e04cf
shanelevites <srlevit94@gmail.com>
2023-01-23 22:46:34 -0800

    MVP fully met with no errors

--- server.js
+++ server.js
@@ -2,37 +2,26 @@
 const express = require('express');
 const path = require('path');
 const uuid = require('./helpers/uuid');
-const fsUtils = require('./helpers/fsUtils');
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// destination file for notes
-const notes = require('./db/db.json');
 
 //express middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// GET Route for homepage
-app.get('/', (req, res) => 
-  res.sendFile(path.join(__dirname,'./public/index.html'))
-);
-
 // -- [GET /notes] should return the notes.html file.
-app.get('/notes', (req, res) => 
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+});
 
 // -- [GET /api/notes] should read the db.json file and return all saved notes as JSON.
-app.get('/api/notes', (req, res) => res.json(notes));
-
-// -- [GET *] should return the index.html file.
-app.get('*', (req, res) => 
-  res.sendFile(path.join(__dirname,'./public/index.html'))
-);
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 // -- [POST /api/notes] adds new note to db.json
 app.post('/api/notes', (req, res) => {
@@ -40,7 +29,7 @@ app.post('/api/notes', (req, res) => {
   console.log(req.body);
 
   const { title, text } = req.body;
-
+  //Makes new note and assigns id, if title and text are not empty
   if (req.body) {
     const newNote = {
       title,
@@ -48,16 +37,20 @@ app.post('/api/notes', (req, res) => {
       id: uuid(),
     };
 
-    fsUtils.readAndAppend(newNote, './db/db.json');
+    //calls fsUtils to append note to JSON file
+    readAndAppend(newNote, './db/db.json');
     res.json('note added successfully');
   } else {
     res.error('Error in adding note');
   }
 });
 
-// reads PORT and logs link to application to console
-app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
+// -- [GET *] should return the index.html file.
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname,'./public/index.html'))
 );
 
-
+// reads PORT and logs link to application to console
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
