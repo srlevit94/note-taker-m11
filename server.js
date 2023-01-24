3815bce3f11e7609bb3bd29c466ee6a8d3f86805
shanelevites <srlevit94@gmail.com>
2023-01-23 18:21:38 -0800

    cleaned up redundant code, MVP met, TODO: bonus criteria for delete functionality

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 // packages needed to run appliation
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-// const api = require('./routes/index.js');
-// const notesRoute = require('./routes/notes.js')
 const uuid = require('./helpers/uuid');
 const fsUtils = require('./helpers/fsUtils');
 
-
-// const notesRoute = require('./routes/notes.html')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,10 +14,7 @@ const notes = require('./db/db.json');
 //express middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-app.use(clog);
-// app.use('/api', api);
-// app.use('/', notesRoute);
+app.use(express.json());
 
 // GET Route for homepage
 app.get('/', (req, res) => 
@@ -34,10 +26,6 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
-);
-
 // -- [GET /api/notes] should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => res.json(notes));
 
@@ -46,6 +34,7 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname,'./public/index.html'))
 );
 
+// -- [POST /api/notes] adds new note to db.json
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   console.log(req.body);
@@ -66,31 +55,9 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-// // -- [POST /api/notes] should receive a new note to save on the request body, add it to the [db.json] file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into [npm] packages that could do this for you).
-// app.post('/api/notes', (req, res) => {
-//     // Log that a POST request was received
-//     console.info(`${req.method} request received to add a note`);
-  
-//     // Destructuring assignment for the items in req.body
-//     const { noteTitle, noteContent } = req.body;
-  
-//     // If all the required properties are present
-//     if (noteTitle  && noteContent) {
-//       // Variable for the object we will save
-//       const newNote = {
-//         noteTitle,
-//         noteContent,
-//         note_id: uuid(),
-//       };
-  
-//       const response = {
-//         status: 'success',
-//         body: newNote,
-//       };
-  
-//       console.log(response);
-//       res.status(201).json(response);
-//     } else {
-//       res.status(500).json('Error in creating Note');
-//     }
-//   });
+// reads PORT and logs link to application to console
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`)
+);
+
+
